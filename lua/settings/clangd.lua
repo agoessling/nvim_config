5f51006b40e1ff36f54cd1e592e4e213b3194e0e
Andrew Goessling <agoessling@gmail.com>
2023-07-14 20:25:58 -0700

    Add clangd flag configuration.

---
+++ lua/settings/clangd.lua
@@ -0,0 +1,8 @@
+local M = {
+  cmd = {
+    "clangd",
+    -- "--query-driver=/home/agoessling/magpie/external/bazel_bootlin/toolchains/tool_wrappers/aarch64/2021.11-1/aarch64-linux-gnu-2021.11-1-gcc",
+  }
+}
+
+return M
